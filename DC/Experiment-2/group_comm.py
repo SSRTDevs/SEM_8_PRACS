62927ce228fec1372ba8123e215412f46d367c50
Abhishek Sharma <abhishekalien09@gmail.com>
2023-04-22 19:34:16 +0530

    Add ADS Files

--- DC/Experiment-2/group_comm.py
+++ DC/Experiment-2/group_comm.py
@@ -1,33 +1,68 @@
-'''
-Group communication typically involves multiple processes or nodes communicating
-with each other simultaneously. One way to implement group communication is by 
-using multicast sockets, which allow messages to be sent to a group of recipients at once.
-The multicast address 224.0.0.0 to 239.255.255.255 is the range of addresses reserved for multicast use.
-This address is not assigned to any particular organization or service, and it is not likely to be used by other applications.
-'''
-
 import socket
-import struct 
+import threading
 
-# Set up the multicast address and port
-multicast_group = ('224.3.29.71', 10000)
-
-# Create a UDP socket and bind it to the local address
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('localhost', 0))
-
-# Set the time-to-live (TTL) for multicast packets
-ttl = struct.pack('b', 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-message = "Hey Bhai"
-sock.sendto(message.encode(), multicast_group)
-
-while True:
-
-    # Send a message to the multicast group
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((host, port))
+        self.clients = []
     
+    def listen(self):
+        self.socket.listen()
+        print(f"Server is listening on {self.host}:{self.port}")
+        while True:
+            client_socket, client_address = self.socket.accept()
+            print(f"New client connected: {client_address}")
+            self.clients.append(client_socket)
+            thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            thread.start()
+    
+    def handle_client(self, client_socket):
+        while True:
+            message = client_socket.recv(1024)
+            for client in self.clients:
+                if client != client_socket:
+                    client.sendall(message)
+                
 
-    # Receive messages sent to the multicast group
-    data, address = sock.recvfrom(1024)
-    print(f'Received {data.decode()} from {address}')
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def connect(self):
+        self.socket.connect((self.host, self.port))
+        print(f"Connected to {self.host}:{self.port}")
+    
+    def send_message(self, message):
+        self.socket.sendall(message.encode("utf-8"))
+    
+    def receive_messages(self):
+        while True:
+            message = self.socket.recv(1024).decode("utf-8")
+            print(message)
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 8000
+    
+    server = Server(host, port)
+    thread = threading.Thread(target=server.listen)
+    thread.start()
+    
+    client1 = Client(host, port)
+    client1.connect()
+    client2 = Client(host, port)
+    client2.connect()
+    
+    thread = threading.Thread(target=client1.receive_messages)
+    thread.start()
+    
+    thread = threading.Thread(target=client2.receive_messages)
+    thread.start()
+    
+    client1.send_message("Hello from client 1")
+    client2.send_message("Hello from client 2")
